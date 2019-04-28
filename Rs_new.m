69bd4ea9fc0aa3faf832d30e3e3029d6c715604d
Ricky Rueda <ricardo.rda97@gmail.com>
2019-04-28 15:37:39 -0700

    HACK

--- Rs_new.m
+++ Rs_new.m
@@ -2,12 +2,14 @@
 function [Rs, Rl_new, Ru_new] =Rs_new(Rl_old,Ru_old,t)
 %filename:   Rs_new.m
 global Rl Ru Tstand;
-Rs=(Rl*Ru)/(Rl+Ru)
+Rs=(Rl*Ru)/(Rl+Ru);
 if t<Tstand
     Rl=1.5*Ru
 end
 if t==Tstand
     Rl=2*Ru
 end
+Rl_new = Rl;
+Ru_new = Ru;
 end
 
