de6344134bcbda737a74e5540006524ff4b9af1b
Ricci-C-Lam <rcl008@ucsd.edu>
2019-04-28 14:21:44 -0700

    Update V.m

--- V.m
+++ V.m
@@ -1,9 +1,8 @@
-function [vl, vu, vo] = V(t)
+function [vl, vu] = V(t)
 global Csa QAo rl ru;
 if t<tstand
     vl = 2.5;
     vu = 2.5;
-    vo = 5;
 else
     vl = QAo(t)*Csa*rl;
     vu = 5 - vl;
