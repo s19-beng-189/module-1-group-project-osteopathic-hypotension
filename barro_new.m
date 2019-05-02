14cdd9fe0faf34bd2403da767668ebcb3684a0d2
Ricky Rueda <ricardo.rda97@gmail.com>
2019-05-02 14:26:36 -0700

    Edited for Classic OH Experiment

--- barro_new.m
+++ barro_new.m
@@ -3,11 +3,11 @@ function [F_new, Psv_new, Rs_new] = barro_new(F, SP, t)
 % adjusts Heart Rate, Systemic Resistance Systemic and Venous Pressure through SNS pathway
 global Psv Rs Tstand;
 thr_SP = 120;          % Keep regulating until threshold systolic pressure is reached
-lag    =  1;           % delay in min
+lag    =  0;           % delay in min
 alphaF =  0.05;        % Recovery Rate Constant for Heart Rate
 alphaP =  0.0005;      % Recovery Rate Constant for Psv
 alphaR =  0.1 ;        % Recovery Rate Constant for Rs
-alphaR = 0; %Test for seperate compensations
+%alphaR = 0; %Test for seperate compensations
 %alphaF = 0;
 %alphaP = 0;
 if SP < thr_SP && t>lag+Tstand         % Adjust until normal levels are reached (120 mmHg) Adjust based on experiemtn
